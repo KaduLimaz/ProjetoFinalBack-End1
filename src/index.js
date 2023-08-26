9cd636fe714e1ae5dde4ac6b50e73c6813a7975e
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2023-08-26 16:16:53 -0300

    Criando Conta e validação de email
    - Criar a conta
       - validar se o campo email ta preenchido
       - validar se o email ja esta cadastrado

--- src/index.js
+++ src/index.js
@@ -5,10 +5,57 @@ import bcrypt from 'bcrypt';
 const app = express();
 const port = 8080;
 
-app.get('/', (require, response) => {
+app.use(express.json());
+
+app.get('/', (request, response) => {
     response.send('Server Online');
 })
 
 app.listen(port,()=>{
     console.log("Server Online");
 });
+
+//banco de dados
+
+const bankUsers = [];
+const messages = [];
+const newUserID = uuidv4()
+
+//casdastro de usuario
+
+app.post("/sign-up", async (request, response) =>{
+    const { name, email, password } = request.body;
+//validação de email
+
+    if(!email){
+        return response.status(400).json({
+            message: 'E-mail obrigatório!'
+        })
+    }
+
+    if(email){
+        const userExists = bankUsers.find(user => user.email === email)
+        if(userExists){
+            return response.status(400).json({
+                message: 'E-mail já cadastrado!'
+            })
+        }
+    
+    }
+
+    const hasPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+        id: newUserID,
+        name,
+        email,
+        password: hasPassword,
+    };
+
+    bankUsers.push(newUser);
+
+    response.status(201).json({
+        message: 'Usuário criado com sucesso!',
+        user: newUser
+    });
+})
