ef29a5f40059cc4e0a8b535852e93135da12aa58
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2023-08-27 13:39:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// import express, { json } from "express";
-const express = require("express");
+import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 
